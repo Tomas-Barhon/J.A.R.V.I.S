6e16315f8e58c40cd35e87f689c41480c7e86f5e
Tomáš Barhoň <tomas.barhon@hotmail.cz>
2024-11-21 08:05:28 +0100

    Adding request limits to JARVIS

--- app/brain.py
+++ app/brain.py
@@ -7,14 +7,31 @@ import os
 from typing import Dict, List
 from dotenv import load_dotenv
 from openai import OpenAI
-
+import time
 class JARVIS:
-    DEFAULT_SYSTEM_PROMPT : Dict = {"role": "system", "content": "You are a helpful assistant."}
+    API_REQUEST_LIMIT = 10
+    API_REQUESTS_PM = 0
+    DEFAULT_SYSTEM_PROMPT : Dict = {
+    "role": "system",
+    "content": (
+        "You are J.A.R.V.I.S., a sophisticated AI assistant from the Iron Man universe. "
+        "You respond in a polite, articulate, and formal tone, offering intelligent insights "
+        "and practical assistance while maintaining a composed and witty demeanor."
+    )
+}
     
     def __init__(self) -> None:
         #load .env variables
         load_dotenv()
         self.open_ai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+        self.start_time = time.time()
+    def reset_requests(self):
+        """Resets the request count after a minute has passed."""
+        current_time = time.time()
+        elapsed_time = current_time - self.start_time
+        if elapsed_time >= 60:
+            JARVIS.API_REQUESTS_PM = 0
+            self.start_time = current_time
 
     def send_prompt(self, messages: List[Dict[str,str]],model :str = "gpt-4o-mini") -> None:
         """Sends messages to the OpenAi model based on specification.
@@ -26,13 +43,19 @@ class JARVIS:
         Returns:
             _type_: _description_
         """
-        request = self.open_ai_client.chat.completions.create(
-            model=model,
-            messages=messages
-        )
-        return request.choices[0].message.content.strip()
-
-
-
+        #Check whether to reset requests per minute
+        self.reset_requests()
+        
+        
+        JARVIS.API_REQUESTS_PM += 1
+        if JARVIS.API_REQUESTS_PM <= JARVIS.API_REQUEST_LIMIT:
+            request = self.open_ai_client.chat.completions.create(
+                model=model,
+                messages=messages
+            )
+            return request.choices[0].message.content.strip()
+        else:
+            return f"""Limit of {JARVIS.API_REQUEST_LIMIT}
+        requests per minute has been reached."""
 
 
--- app/main.py
+++ app/main.py
@@ -1,31 +1,48 @@
-import speech_recognition as sr
 from ctypes import *
+import speech_recognition as sr
 from speech_to_text import Speech_Recognition
+from brain import JARVIS
 
-#Disabling audio warnings
-#Credit to "https://blog.yjl.im/2012/11/pyaudio-portaudio-and-alsa-messages.html" for solving the warnings
-ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+
+LISTEN = True
+
 def py_error_handler(filename, line, function, err, fmt):
     pass
-c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
-asound = cdll.LoadLibrary('libasound.so')
-asound.snd_lib_error_set_handler(c_error_handler)
+def disable_libasound_warnings():
+    """Disabling audio warnings
+    Credit to "https://blog.yjl.im/2012/11/pyaudio-portaudio-and-alsa-messages.html" for solving the warnings
+"""
+    ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+    c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+    asound = cdll.LoadLibrary('libasound.so')
+    asound.snd_lib_error_set_handler(c_error_handler)
 
+def main():
+    """
+    Main loop waiting for speech input.
+    """
 
+    speech_recognition = Speech_Recognition()
+    recognizer = sr.Recognizer()
+    jarvis = JARVIS()
 
-
-speech_recognition = Speech_Recognition()
-r = sr.Recognizer()
-while(True):
-    # Exception handling to handle
-    # exceptions at the runtime
-    try:
+    while(True):
         
-        print(speech_recognition.transcribe_input(r))
-        
+        try:
+            if LISTEN:
+                user_input = speech_recognition.transcribe_input(recognizer)
+                print("Received user input: ",user_input)
+                response = jarvis.send_prompt(
+                    messages = [JARVIS.DEFAULT_SYSTEM_PROMPT,
+                {"role": "user", "content": user_input}]
+                )
+                print(response)
+        except sr.RequestError as e:
+            print(e)
             
-    except sr.RequestError as e:
-        print(e)
-        
-    except sr.UnknownValueError as e:
-        print(e)
+        except sr.UnknownValueError as e:
+            print(e)
+
+if __name__ == "__main__":
+    disable_libasound_warnings()
+    main()
